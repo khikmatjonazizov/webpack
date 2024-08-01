e500b6d5ad1e486f000f871c5ee185ac63fe97b5
khikmatjon <azizovxikmatjon04@gmail.com>
2024-08-01 19:53:31 +0500

    HtmlWebpackPlugin, ProgressPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
-const path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack'); 
 
 module.exports = (env) => {
   return {
@@ -9,5 +11,9 @@ module.exports = (env) => {
       clean: true
     },
     mode: env.mode ?? 'development',
+    plugins: [
+      new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }),
+      new webpack.ProgressPlugin()
+    ]
   }
 } 
